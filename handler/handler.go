9eb87fc2a33c4650f5d5b5d82bbead23f668aea5
WeiWeiCheng123 <wxyz41055@gmail.com>
2022-02-10 20:35:03 +0800

    add handler and fix some bugs

--- handler/handler.go
+++ handler/handler.go
@@ -4,21 +4,24 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/WeiWeiCheng123/URLshortener/store"
 )
 
 func Build() *gin.Engine {
 	router := gin.Default()
-	router.POST("/api/urls", Shorten())
+	router.POST("/api/urls", Shorten)
 	router.GET("/:shortURL", Parse)
-	router.Run()
+	router.Run(":8080")
 	return router
 }
 
-func Shorten() gin.HandlerFunc {
-	return nil
+func Shorten(c *gin.Context) {
+	url := c.DefaultPostForm("","")
+	fmt.Println(url)
 }
 
 func Parse(c *gin.Context) {
 	shortURL := c.Param("shortURL")
+	store.Load()
 	fmt.Println(shortURL)
 }
