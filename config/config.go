db8d96f19190549136a7377f4dcbe99275ceb2ac
WeiWeiCheng123 <wxyz41055@gmail.com>
2022-02-19 19:39:47 +0800

    env file and config

---
+++ config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+func GetStr(key string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		panic("Error to get" + key)
+	}
+
+	return val
+}
+
+func GetInt(key string) int {
+	val := os.Getenv(key)
+	if val == "" {
+		panic("Error to get" + key)
+	}
+
+	val_int, err := strconv.Atoi(val)
+	if err != nil {
+		panic("Error to get" + key)
+	}
+
+	return val_int
+}
