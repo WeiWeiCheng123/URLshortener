79ced8ee83a98ef6ef86995cbd2b7a5a88c15529
WeiWeiCheng123 <wxyz41055@gmail.com>
2022-02-27 18:15:01 +0800

    change the id generate way

--- lib/function/id_generator.go
+++ lib/function/id_generator.go
@@ -32,7 +32,7 @@ func Generator() string {
 	t := uint64(time.Now().Unix())
 	rand.Seed(time.Now().UnixNano())
 	r_1 := rand.Uint64()
-	rand.Seed(time.Now().Unix())
+	rand.Seed(time.Now().UnixNano() + int64(r_1))
 	r_2 := uint64(rand.Int())
 	t = t + r_1 + r_2
 
