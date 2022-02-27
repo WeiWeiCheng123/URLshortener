bab91298541e3ff10ecb80e5a6ce2bf5c408f648
WeiWeiCheng123 <wxyz41055@gmail.com>
2022-02-27 15:43:22 +0800

    tidy up

--- lib/cron/cron.go
+++ lib/cron/cron.go
@@ -1,4 +1,5 @@
 package cron
+
 import (
 	"github.com/WeiWeiCheng123/URLshortener/model"
 	"github.com/robfig/cron"
@@ -6,8 +7,20 @@ import (
 
 func Del_Expdata() {
 	c := cron.New()
-	c.AddFunc("0 * * * *", 
+	//Demo用，所以設置成每5分鐘進行刪除
+	c.AddFunc("*/5 * * * *",
 		model.Pg_Del_Exp,
 	)
+	/*
+		實際上使用，我的設計會是在每天的最冷門時段進行刪除
+		在這邊我設置成凌晨三點
+		 *     *     *      *      *
+		 分    時    日     月     星期
+		0-59  0-23  1-31  1-12  0-6 (週日~週六)
+		c.AddFunc("* 3 * * *",
+			model.Pg_Del_Exp,
+		)
+	*/
+
 	c.Start()
 }
