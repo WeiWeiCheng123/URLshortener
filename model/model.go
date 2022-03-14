6957c34febd456fc742c132153832c8aecbe5df0
WeiWeiCheng123 <wxyz41055@gmail.com>
2022-03-14 23:34:46 +0800

    tidy up

--- model/model.go
+++ model/model.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Shortener struct {
-	ShortId     string    `xorm:pk json:short_id`
-	OriginalUrl string    `json:original_url`
-	ExpireTime  time.Time `json:expire_time`
+	ShortId     string    `xorm:"pk" json:"short_id"`
+	OriginalUrl string    `json:"original_url"`
+	ExpireTime  time.Time `json:"expire_time"`
 }
 
 func (u *Shortener) TableName() string {
