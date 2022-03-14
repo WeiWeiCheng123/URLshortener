5af258cfa505e4084434d6a88e3fe1580f5208f7
WeiWeiCheng123 <wxyz41055@gmail.com>
2022-03-14 23:31:00 +0800

    tidy up

--- model/model.go
+++ model/model.go
@@ -13,31 +13,3 @@ type Shortener struct {
 func (u *Shortener) TableName() string {
 	return "shortener"
 }
-
-//docker rmi url-shortener
-//docker-compose down
-//docker-compose up
-//curl -X POST -H "Content-Type:application/json" -d '{"url":"https://www.dcard.tw/f","expireAt":"2023-01-01T09:00:41Z"}' http://localhost:8080/api/v1/urls
-//curl -L -X GET "http://localhost:8080/U5kPkNw"
-
-/*
-git add .
-git commit -m "fix bug"
-git push origin v2
-*/
-
-/*
-  backend:
-    container_name: 'url-shortener'
-    image: 'url-shortener'
-    build: ./
-    ports:
-      - "8080:8080"
-    restart: always
-    environment:
-      REDIS_HOST: redis:6379
-      DB_HOST: postgres
-    depends_on:
-      - postgres
-      - redis
-*/
