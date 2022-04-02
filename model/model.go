7c5d431b004d1204181580a14d740d00ee3eedde
WeiWeiCheng123 <wxyz41055@gmail.com>
2022-04-02 22:23:48 +0800

    Update model.go

--- model/model.go
+++ model/model.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Shortener struct {
-	ShortId     uint64       `gorm:"pk" json:"short_id"`
+	ShortId     uint64    `gorm:"pk" json:"short_id"`
 	OriginalUrl string    `gorm:"type:varchar(500)" json:"original_url"`
 	ExpireTime  time.Time `json:"expire_time"`
 }
